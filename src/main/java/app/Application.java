0d8b0abb08fb7c512d1f1ebe730fccac40176c57
PoltoryBukvy <102367161+PoltoryBukvy@users.noreply.github.com>
2022-05-25 23:25:53 +0300

    Commit

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,23 @@
+package app;
+
+import app.config.AppConfig;
+import app.model.AnimalsCage;
+import app.model.Timer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        for (int i = 0; i < 5; i++) {
+            AnimalsCage bean = applicationContext.getBean(AnimalsCage.class);
+            System.out.println(bean.getTimer().getTime());
+            //bean.whatAnimalSay();
+            /*Timer bean = applicationContext.getBean(Timer.class);
+            System.out.println(bean.getTime());*/
+        }
+    }
+
+}
